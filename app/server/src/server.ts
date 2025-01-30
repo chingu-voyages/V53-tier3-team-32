2500d92626b194326db0ef4f02e75d0b5a11b53c
devim <developerimalka@gmail.com>
2025-01-30 15:02:12 +0530

    Fix missing dependecies

--- app/server/src/server.ts
+++ app/server/src/server.ts
@@ -3,7 +3,8 @@ import { IndexRoute } from "./routes";
 import DB from "./lib/DB";
 import { authroute } from "./routes/auth";
 import { json } from "body-parser";
-const cookieSession = require("cookie-session");
+import passport from 'passport';
+const session  = require('express-session');
 require("dotenv").config();
 
 DB;
@@ -11,13 +12,14 @@ DB;
 const app = express();
 const port: number = 3000;
 
+app.use(passport.initialize());
+
 app.use(
-  cookieSession({
-    // age of the cookie in milliseconds
-    // cookie will last for one day
-    maxAge: 24 * 60 * 60 * 1000,
-    // encrypts the user id
-    keys: [process.env.COOKIEKEY],
+  session({
+ 	secret: process.env.COOKIEKEY,
+	resave: false,
+	saveUninitialized: true,
+	cookie: {secure: true}
   }),
 );
 
@@ -27,4 +29,4 @@ app.use("/auth", authroute);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
-});
+}); 
